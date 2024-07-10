604a71ce28b00f15e86316ebea121a60876b67ca
Jeevamk <138573828+Jeevamk@users.noreply.github.com>
2024-07-10 17:56:16 +0530

    navbar completed

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,96 @@
-import React from 'react'
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
+import "../../styles/header.css";
 
-const Header = () => {
+
+
+export default function Header() {
   return (
-    <div>Header</div>
-  )
+    <header className="shadow sticky z-50 top-0">
+      <nav className="bg-white border-gray-200 px-4 lg:px-6 py-2.5">
+        <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
+          <Link to="/" className="flex items-center">
+            <img
+              src="./icon.svg"
+              
+              alt="Logo"
+            />
+            <span className="logo-name">Travlog</span>
+          </Link>
+          <div className="flex items-center lg:order-2">
+            <Link
+              to="#"
+              className="login"
+            >
+              Log In
+            </Link>
+            <Link
+              to="#"
+              className="signup"
+            >
+              Sign Up
+            </Link>
+          </div>
+          <div
+            className="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1 "
+            id="mobile-menu-2"
+          >
+            <ul className="nav-links-container">
+              <li>
+                <NavLink
+                  to="/"
+                  className={({ isActive }) =>
+                    `nav-link ${
+                      isActive ? "nav-link-active" : "nav-link-inactive"
+                    } nav-link-hover`
+                  }
+                >
+                  Home
+                </NavLink>
+              </li>
+              <li>
+              <NavLink
+                  to="/discover"
+                  className={({ isActive }) =>
+                    `nav-link ${
+                      isActive ? "nav-link-active" : "nav-link-inactive"
+                    } nav-link-hover`
+                  }
+                >
+                  Discover
+                </NavLink>
+              </li>
+              <li>
+              <NavLink
+                  to="/deal"
+                  className={({ isActive }) =>
+                    `nav-link ${
+                      isActive ? "nav-link-active" : "nav-link-inactive"
+                    } nav-link-hover`
+                  }
+                >
+                  Special Deals
+                </NavLink>
+              </li>
+              <li>
+              <NavLink
+                  to="/contact"
+                  className={({ isActive }) =>
+                    `nav-link ${
+                      isActive ? "nav-link-active" : "nav-link-inactive"
+                    } nav-link-hover`
+                  }
+                >
+                  Contact
+                </NavLink>
+              </li>
+              <li>
+              
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </header>
+  );
 }
-
-export default Header
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
+import Layout from './Layout.jsx'
+import Home from './components/Home/Home.jsx'
+
+
+
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<Layout/>}>
+      <Route path='/' element={<Home/>}/>
+      <Route path='*' element={<div className='text-3xl font-semibold bg-slate-100 text-center py-5'>Not Found</div>}/>
+    </Route>
+  )
+)
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
